647d59e129cd2ebc1b77de1868fdf4c9140f9b43
zxy01 <2326674367@qq.com>
2018-01-02 18:27:34 +0800

    fix bug:登录过期

--- js/login.js
+++ js/login.js
@@ -16,9 +16,13 @@ $(function() {
                     if(data.data.token_valid_time > 0){
                         //设置过期时间
                         exp.setTime(exp.getTime() + 1000 * 60 * data.data.token_valid_time); //计算毫秒
+                        document.cookie = "token=" + data.token + ";expires=" + exp.toGMTString();
+                        document.cookie = "data=" + JSON.stringify(data.data) + ";expires=" + exp.toGMTString();
                     }
-                    document.cookie = "token=" + data.token + ";expires=" + exp.toGMTString();
-                    document.cookie = "user=" + JSON.stringify(data.data) + ";expires=" + exp.toGMTString();
+                    else {
+                        document.cookie = "token=" + data.token;
+                        document.cookie = "data=" + JSON.stringify(data.data);
+                    }
                     //跳转个人中心页面
                     window.location.href = 'info.html';
                 }
