00f3b71fffedbba254d95169721c9f86eec81740
zxy01 <2326674367@qq.com>
2018-02-05 13:56:40 +0800

    订单详情页面

--- js/order/loadorder.js
+++ js/order/loadorder.js
@@ -28,8 +28,20 @@ function loadorder(pid,oid,flag) {
                         .replace('{range}',data.range);
                     $("#success").append(success_part);
                 }
+                //加载订单详情
                 else {
-
+                    var detail_part = $('#detail_change').html();
+                    detail_part = detail_part.replace('{username}',data.username)
+                        .replace('{department}',data.department)
+                        .replace('{doctor_name}',data.doctorname)
+                        .replace('{price}',data.price)
+                        .replace('{id}',data.id)
+                        .replace('{status}',data.status)
+                        .replace('{type}',data.typename)
+                        .replace('{create_time}',data.create_time)
+                        .replace('{date}',data.appointment_date)
+                        .replace('{time}',data.range);
+                    $("#order-info").append(detail_part);
                 }
             }
 
