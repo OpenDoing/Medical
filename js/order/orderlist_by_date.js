41e63f32eaee4332bba4e4252cd5d729873a138b
zxy01 <2326674367@qq.com>
2018-05-07 22:35:43 +0800

    医生订单列表查询&首页

---
+++ js/order/orderlist_by_date.js
@@ -0,0 +1,127 @@
+window.onload = init_order_table();
+
+function init_orderlist(date) {
+    $.ajax({
+        type: "GET",
+        url: config.base_url + "Doctororders",
+        data:{
+            'date':date,
+            'token':checktoken(),
+            'flag': 0
+        },
+        success: function (data) {
+            if (data.succ == 1){
+                for (var i = 0; i < data.data.length; i++) {
+                    if (data.data[i].sex === 0) {
+                        data.data[i].sex = "女";
+                    } else {
+                        data.data[i].sex = "男";
+                    }
+                }
+                $('#order').bootstrapTable('load',data.data);
+            }
+        }
+    });
+}
+
+function init_order_table(data) {
+
+    $('#order').bootstrapTable({
+
+        method:"GET",
+        dataType:'json',
+        pagination:true,
+        data:data,
+
+        //url:config.base_url + "order?profile_id=2&token="+getCookie('token'),
+
+        onLoadSuccess: function(){  //加载成功时执行
+            return "加载成功";
+        },
+        onLoadError: function(){  //加载失败时执行
+            return "加载数据失败";
+        },
+        striped:true,
+        responseHandler:function (res) {    //data prehandle
+
+
+        },
+        onClickRow:function (row) {
+
+        },
+        columns: [
+            {
+                field: 'id',
+                title: '订单编号',
+                align: 'center',
+                width: 50 + "px"
+            },
+            {
+                field: 'name',
+                title: '患者姓名'
+            },
+            {
+                field: 'sex',
+                title: '患者性别'
+            },
+            {
+                field: 'birth',
+                title: '患者生日'
+            },
+            {
+                field: 'appointment_date',
+                title: '预约时间'
+            },
+            {
+                field: 'price',
+                title: '价格'
+            },
+            {
+                field: 'status',
+                title: '状态'
+            },
+            {
+                title: '操作',
+                align: "center",
+                events: "operateEvents",
+                formatter: function (value, row, index) {
+                    return [
+                        '<button id="showinfo" type="button" class="btn btn-default">查看</button>'
+                    ].join("");
+                }
+            }
+        ]
+    });
+}
+
+function load_profiles() {
+    $.ajax({
+        url:"http://bieke.cf:8080/ma/zxy/api/userprofile",
+        type: 'get',
+        dataType: 'json',
+        data: {
+            profile_id: 0,
+            token:checktoken()
+        },
+        success: function (data) {
+            if(data.succ == 1){
+                var user = data.data;
+                var htmlNodes = '';
+                for(var i in user) {
+                    htmlNodes += '<button class="btn btn-default eborder profile_buttons" value="' + user[i].id + '"> ' + user[i].name + ' </button>';
+                }
+                $('#user_list').empty().append(htmlNodes);
+                $(".profile_buttons").on("click",function () {
+                    init_orderlist($(this).val());
+                    //$('#order').bootstrapTable('refresh',{url:config.base_url + "medicalrecord?profile_id=" + $(this).val() +"&token="+checktoken() + "&record_id=0"});
+                });
+            }
+            else {
+                alert(data.error);
+            }
+
+        }
+
+    });
+}
+
--- js/util.js
+++ js/util.js
@@ -75,3 +75,20 @@ function getdate(day) {
     var month = date.getMonth() + 1;
     return date.getFullYear() + '年' + month + '月' + date.getDate() + '日';
 }
+
+//获取当前日期，格式为YYYY-MM-DD
+function getNowFormatDate() {
+    var date = new Date();
+    var seperator1 = "-";
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var strDate = date.getDate();
+    if (month >= 1 && month <= 9) {
+        month = "0" + month;
+    }
+    if (strDate >= 0 && strDate <= 9) {
+        strDate = "0" + strDate;
+    }
+    var currentdate = year + seperator1 + month + seperator1 + strDate;
+    return currentdate;
+}
