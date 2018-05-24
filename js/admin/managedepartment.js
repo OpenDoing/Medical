71d9dea538baf55820a145bda20603092cd11c4b
zxy01 <2326674367@qq.com>
2018-05-24 14:24:15 +0800

    科室&医生类型管理

--- js/admin/managedepartment.js
+++ js/admin/managedepartment.js
@@ -1,77 +1,15 @@
-window.onload = init();
+window.onload = init_de_table();
 
-function init() {
-    init_doctor_table();
-    init_select();
-}
+function init_de_table() {
 
-function init_select() {
-    $.ajax({
-        type: "POST",
-        url: config.base_url + "department",
-
-        success: function (data) {
-            if(data.succ == 1){
-                var data = data.data;
-                var htmls = "";
-                for(var i in data){
-                    htmls += "<option value=" + data[i].id + ">" + data[i].name + "</option>";
-                }
-                $("#deid1").empty().append(htmls);
-                $("#deid2").empty().append(htmls);
-            }
-            else {
-                alert(data.error);
-            }
-        }
-    });
-    $.ajax({
-        type: "POST",
-        url: config.base_url + "doctortype",
-
-        success: function (data) {
-            if(data.succ == 1){
-                var data = data.data;
-                var htmls = "";
-                for(var i in data){
-                    htmls += "<option value=" + data[i].id + ">" + data[i].type + "</option>";
-                }
-                $("#type1").empty().append(htmls);
-                $("#type2").empty().append(htmls);
-            }
-            else {
-                alert(data.error);
-            }
-        }
-    });
-}
-
-function init_doctor_list() {
-    $.ajax({
-        type: "GET",
-        url: config.base_url + "Doctorprofile/name",
-        data:{
-            'name':$("#search_name").val()
-        },
-        success: function (data) {
-            if (data.succ == 1){
-                $('#doctors').bootstrapTable('load',data.data);
-                $(".fixed-table-loading")[0].style.display="none";      //数据加载成功  加载那行字去掉
-            }
-        }
-    });
-}
-
-function init_doctor_table(data) {
-
-    $('#doctors').bootstrapTable({
+    $('#departments').bootstrapTable({
 
         method:"GET",
         dataType:'json',
         pagination:true,
-        data:data,
+        search: true,
 
-        //url:config.base_url + "order?profile_id=2&token="+getCookie('token'),
+        url:config.base_url + "department/detaillist",
 
         onLoadSuccess: function(){  //加载成功时执行
 
@@ -85,25 +23,17 @@ function init_doctor_table(data) {
         columns: [
             {
                 field: 'id',
-                title: '医生编号',
+                title: '科室编号',
                 align: 'center',
                 width: 50 + "px"
             },
             {
                 field: 'name',
-                title: '医生姓名'
+                title: '科室名'
             },
             {
-                field: 'department',
-                title: '科室'
-            },
-            {
-                field: 'typename',
-                title: '医生类型'
-            },
-            {
-                field: 'introduction',
-                title: '医生简介'
+                field: 'description',
+                title: '科室介绍'
             },
             {
                 title: '操作',
@@ -111,7 +41,7 @@ function init_doctor_table(data) {
                 events: "operateEvents",
                 formatter: function (value, row, index) {
                     return [
-                        '<button id="edit" type="button" class="btn btn-default" data-toggle="modal" data-target="#edit-doctor-modal">编辑</button>'
+                        '<button id="edit" type="button" class="btn btn-default" data-toggle="modal" data-target="#edit-modal">编辑</button>'
                     ].join("");
                 }
             }
@@ -125,23 +55,18 @@ function create() {
 
     $.ajax({
         type: "POST",
-        url: config.base_url + "doctorprofile/create",
+        url: config.base_url + "department/create",
         data: {
             'token':token,
-            'nickname':$("#nickname").val(),
-            'password':$("#password").val(),
-            'phone':$("#phone").val(),
             'name':$("#name1").val(),
-            'deid':$("#deid1").val(),
-            'type':$("#type1").val(),
-            'introduction':$("#introduction1").val()
+            'description':$("#description1").val()
         },
         success: function (data) {
             $("#bOK1").attr("disabled",false).text("提交");
             if(data.succ == 1){
                 var succ_message = "<div class=\"alert alert-success alert-dismissible\" role=\"alert\">\n" +
                     "                        <button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>\n" +
-                    "                        医生账号添加成功,一秒后自动刷新本页面\n" +
+                    "                        科室添加成功,一秒后自动刷新本页面\n" +
                     "                    </div>";
                 $("#alertmessage1").append(succ_message);
                 //页面需要刷新
@@ -168,28 +93,26 @@ function update() {
 
     $.ajax({
         type: "POST",
-        url: config.base_url + "doctorprofile/update",
+        url: config.base_url + "department/update",
         data: {
             'token':token,
-            'doctor_id':$('#doctor_id').val(),
+            'department_id':$('#d_id').val(),
             'name':$("#name2").val(),
-            'department_id':$("#deid2").val(),
-            'type':$("#type2").val(),
-            'introduction':$("#introduction2").val()
+            'description':$("#description2").val()
         },
         success: function (data) {
             $("#bOK2").attr("disabled",false).text("提交");
             if(data.succ == 1){
                 var succ_message = "<div class=\"alert alert-success alert-dismissible\" role=\"alert\">\n" +
                     "                        <button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>\n" +
-                    "                        医生账号修改成功\n" +
+                    "                        科室资料修改成功,一秒后自动刷新本页面\n" +
                     "                    </div>";
                 $("#alertmessage2").append(succ_message);
                 //页面需要刷新
                 // location.reload();
-                // setTimeout(function(){  //使用  setTimeout（）方法设定定时2000毫秒
-                //     window.location.reload();//页面刷新
-                // },1500);
+                setTimeout(function(){  //使用  setTimeout（）方法设定定时2000毫秒
+                    window.location.reload();//页面刷新
+                },1500);
             }
 
             else {
@@ -204,12 +127,9 @@ function update() {
 
 window.operateEvents = {
     'click #edit': function (e, value, row, index) {
-        console.log(row);
         $('#name2').val(row.name);
-        $('#introduction2').val(row.introduction);
-        $('#deid2').val(row.department_id);
-        $('#type2').val(row.type);
-        $('#doctor_id').val(row.id);
+        $('#description2').val(row.description);
+        $('#d_id').val(row.id);
     }
 };
 
