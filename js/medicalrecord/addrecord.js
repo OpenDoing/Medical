b898a1db48a3ac7a91382ce15cce6c25aebc13bf
OpenDoing <dyninger@163.com>
2018-05-24 21:38:30 +0800

    fix 2 import bugs： 1.页面加载两次 2.点击查看订单之后再点击病历管理，用户数据不显示问题

--- js/medicalrecord/addrecord.js
+++ js/medicalrecord/addrecord.js
@@ -2,6 +2,55 @@ window.onunload = load_profile();
 
 var record_id,profile_id;
 
+function initImage() {
+    $(function() {
+        $("#imageFile").fileinput({
+            uploadUrl: config.base_url+ "Recordimage/upload" , //上传的地址  ?token="+checktoken()+"&type=doctor
+            language: "zh",
+            showCaption: true,              //是否显示标题
+            uploadAsync: false,
+            showUpload: true,               //是否显示上传按钮
+            showRemove: true,               //是否显示移除按钮
+            showPreview: true,                //是否显示预览按钮
+            allowedFileExtensions: ["jpg", "jpeg","bmp","png"], //接收的文件后缀
+            // previewSettings: {
+            //     image: {width: "100px", height: "100px"},
+            // },
+            // uploadExtraData: {  //上传的时候，增加的附加参数
+            //     token:checktoken(),
+            //     profile_id:profile_id,
+            //     record_id:record_id,
+            //     type_id:$("#type").val()
+            //
+            //
+            //     // folder: '数据导入文件', guid: $("#AttachGUID").val()
+            // },
+            uploadExtraData: function(previewId, index) {   //额外参数的关键点
+                var obj = {};
+                obj.token = checktoken();
+                obj.profile_id = profile_id;
+                obj.record_id = record_id;
+                obj.type_id = $("#type").val();
+
+                console.log(obj);
+                return obj;
+            }
+        })
+            .on('fileuploaded',function (event, data, previewId, index) {
+                var res = data.response;
+                console.log(res);
+                bsStep(3);
+            })
+            .on('filepreupload',function (event, data, previewId, index) {
+                var res = data.response;
+                console.log(res);
+                bsStep(3);
+            })
+
+    });
+}
+
+
 function load_profile() {
     $("#step2").hide();
     var token = checktoken();
@@ -77,6 +126,13 @@ function loadtype() {
                 htmls += "<option value=" + data[i].id + ">" + data[i].type + "</option>";
             }
             $("#type").empty().append(htmls);
+            initImage();
         }
     });
 }
+
+
+$("#select").click(function () {
+    var temp = $("#type").val();
+    console.log(temp);
+})
