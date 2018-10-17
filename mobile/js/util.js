2752618625fbaa19688809af137546120c2feea3
zxy01 <2326674367@qq.com>
2018-10-17 21:21:25 +0800

    提交订单&预约成功（mobile）

--- mobile/js/util.js
+++ mobile/js/util.js
@@ -115,16 +115,4 @@ function getNowFormatDate() {
     return currentdate;
 }
 
-//todo:字体大小自适应
-(function (doc, win) {
-    var docEl = doc.documentElement,
-        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
-        recalc = function () {
-            var clientWidth = docEl.clientWidth;
-            if (!clientWidth) return;
-            docEl.style.fontSize = 20 * (clientWidth / 320) + 'px';
-        };
-    if (!doc.addEventListener) return;
-    win.addEventListener(resizeEvt, recalc, false);
-    doc.addEventListener('DOMContentLoaded', recalc, false);
-})(document, window);
+
